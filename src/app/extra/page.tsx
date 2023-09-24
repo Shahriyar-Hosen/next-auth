a3833fa90e4e1a1df7a6e89dd308a2fb78d7e29e
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2023-09-24 21:14:03 +0600

    ExtraPage

--- src/app/extra/page.tsx
+++ src/app/extra/page.tsx
@@ -1,5 +1,3 @@
-export default async function ExtraPage() {
+const ExtraPage = async () => <h1 className="text-5xl">Extra Page!</h1>;
 
-    return <h1 className="text-5xl">Extra Page!</h1>
-
-}
+export default ExtraPage;
