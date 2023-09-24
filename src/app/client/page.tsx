385e0ae10cf0854cf59ecab9d62eeacd106ea812
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2023-09-24 21:10:34 +0600

    client page done

--- src/app/client/page.tsx
+++ src/app/client/page.tsx
@@ -1,21 +1,24 @@
-'use client'
-// Remember you must use an AuthProvider for 
+"use client";
+
+// Remember you must use an AuthProvider for
 // client components to useSession
-import { useSession } from 'next-auth/react'
-import { redirect } from 'next/navigation'
-import UserCard from '../components/UserCard'
+import { useSession } from "next-auth/react";
+import { redirect } from "next/navigation";
+import UserCard from "../components/UserCard";
 
-export default function ClientPage() {
-    const { data: session } = useSession({
-        required: true,
-        onUnauthenticated() {
-            redirect('/api/auth/signin?callbackUrl=/client')
-        }
-    })
+const ClientPage = () => {
+  const { data: session } = useSession({
+    required: true,
+    onUnauthenticated() {
+      redirect("/api/auth/signin?callbackUrl=/client");
+    },
+  });
 
-    return (
-        <section className="flex flex-col gap-6">
-            <UserCard user={session?.user} pagetype={"Client"} />
-        </section>
-    )
-}
+  return (
+    <section className="flex flex-col gap-6">
+      <UserCard user={session?.user} pagetype={"Client"} />
+    </section>
+  );
+};
+
+export default ClientPage;
