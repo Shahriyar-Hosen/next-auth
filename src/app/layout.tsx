8c4d42ca03de4152e1b789abcfcc779101f5193c
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2023-09-24 21:16:07 +0600

    RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,30 +1,28 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
-import Navbar from './components/Navbar'
-import AuthProvider from './context/AuthProvider'
+import { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { ReactNode } from "react";
+import Navbar from "./components/Navbar";
+import AuthProvider from "./context/AuthProvider";
+import "./globals.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = {
-  title: 'NextAuth Tutorial',
-  description: 'Learn NextAuth.js by Dave Gray',
-}
+export const metadata: Metadata = {
+  title: "NextAuth Tutorial",
+  description: "Learn NextAuth.js by Dave Gray",
+};
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>
-        <AuthProvider>
-          <Navbar />
-          <main className="flex justify-center items-start p-6 min-h-screen">
-            {children}
-          </main>
-        </AuthProvider>
-      </body>
-    </html>
-  )
-}
+const RootLayout = ({ children }: { children: ReactNode }) => (
+  <html lang="en">
+    <body className={inter.className}>
+      <AuthProvider>
+        <Navbar />
+        <main className="flex justify-center items-start p-6 min-h-screen">
+          {children}
+        </main>
+      </AuthProvider>
+    </body>
+  </html>
+);
+
+export default RootLayout;
